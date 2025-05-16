bf7ee8cab01626c586623d7ca0d792c63b19f121
Guoqi Sun <hi@guoqi.dev>
2025-05-16 16:46:46 +0800

    feat: add shadcn-ui component library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import { LatestPost } from '~/app/_components/post'
 import { HydrateClient, api } from '~/trpc/server'
 
+import { Button } from '~/components/ui/button'
+
 export default async function Home() {
   const hello = await api.post.hello({ text: 'from tRPC' })
 
@@ -15,6 +17,7 @@ export default async function Home() {
           <h1 className='font-extrabold text-5xl tracking-tight sm:text-[5rem]'>
             Create <span className='text-[hsl(280,100%,70%)]'>T3</span> App
           </h1>
+          <Button>test</Button>
           <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
             <Link
               className='flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20'
