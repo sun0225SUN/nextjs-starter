1b2129a9e7b2b4e7d44cac77cb8c1aa0d2f11fba
Guoqi Sun <hi@guoqi.dev>
2025-05-16 16:59:44 +0800

    feat: support dark mode with next-themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 
 import { LatestPost } from '~/app/_components/post'
-import { HydrateClient, api } from '~/trpc/server'
-
+import { ThemeToggle } from '~/components/theme/toggle'
 import { Button } from '~/components/ui/button'
+import { HydrateClient, api } from '~/trpc/server'
 
 export default async function Home() {
   const hello = await api.post.hello({ text: 'from tRPC' })
@@ -17,7 +17,8 @@ export default async function Home() {
           <h1 className='font-extrabold text-5xl tracking-tight sm:text-[5rem]'>
             Create <span className='text-[hsl(280,100%,70%)]'>T3</span> App
           </h1>
-          <Button>test</Button>
+          <Button>shadcn ui Button</Button>
+          <ThemeToggle />
           <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8'>
             <Link
               className='flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20'
