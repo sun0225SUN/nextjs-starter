323f284db1adf3682d9653f13f379ff124a94c74
Guoqi Sun <hi@guoqi.dev>
2025-05-16 18:50:39 +0800

    feat: use next-intl to achieve multi-language functionality without i18n routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import '~/styles/globals.css'
 import type { Metadata } from 'next'
 import { Geist } from 'next/font/google'
 
+import { NextIntlClientProvider } from 'next-intl'
+import { getLocale } from 'next-intl/server'
 import { ThemeProvider } from '~/components/theme/provider'
 import { TRPCReactProvider } from '~/trpc/react'
 
@@ -17,25 +19,29 @@ const geist = Geist({
   variable: '--font-geist-sans',
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const locale = await getLocale()
+
   return (
     <html
-      lang='en'
+      lang={locale}
       suppressHydrationWarning
       className={`${geist.variable}`}
     >
       <body>
         <TRPCReactProvider>
-          <ThemeProvider
-            attribute='class'
-            defaultTheme='system'
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
+          <NextIntlClientProvider>
+            <ThemeProvider
+              attribute='class'
+              defaultTheme='system'
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+          </NextIntlClientProvider>
         </TRPCReactProvider>
       </body>
     </html>
